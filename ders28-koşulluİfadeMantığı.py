879e525490b9c5398edb9ea9cd432af469dad3a9
Yasin <yasin.yilmaz@berqnet.com>
2025-09-13 16:12:04 +0300

    bool ve if

---
+++ ders28-koşulluİfadeMantığı.py
@@ -0,0 +1,7 @@
+if 70<7:
+    print("ilk sayi ikinci sayidan küçüktür.")
+elif 70==7:
+    print("ilk sayi ikinci sayi ile eşittir.")
+else:
+    print("ilk sayi ikinci sayidan ne küçüktür ne de eşittir.")
+    
---
+++ ders29-boolVeriTürü.py
@@ -0,0 +1,21 @@
+#bool true yada falsedır.
+x=bool("elcinn")
+y=bool(7)
+z=bool( )
+a=bool(0)
+print(x)
+print(y)
+print(z)
+print(a)
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(a))
+print("_______________")
+if 0:
+    print("yes")
+else:
+    print("no")
+
+b=77
+print(isinstance(b,int))
