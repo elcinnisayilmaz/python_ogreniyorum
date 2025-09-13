6394c2ef51bafc39c8321c25da1c5c54fc729b02
Yasin <yasin.yilmaz@berqnet.com>
2025-09-13 19:05:11 +0300

    aritmetik ve atama operatörleri

---
+++ ders30-aritmetikVeAtamaOperatörleri.py
@@ -0,0 +1,47 @@
+#+
+number1=12
+nubmer2=7
+
+print(number1+nubmer2)
+
+#-
+
+print(number1-nubmer2)
+
+#*
+
+print(number1*nubmer2)
+
+#/
+
+print(number1/nubmer2)
+
+#% bir bölme işleminin kalanını verir
+
+print(number1%nubmer2)
+
+#** iki sayıyı üslü kabul eder ve üslü olarak çarpar
+
+print(number1**nubmer2)
+
+#// bölme işlemindeki .dan sonraki küsüratı saymaz
+
+print(number1//nubmer2)
+
+#+= içerideki veriyi dışaridaki veri ile toplar ve sonucu variablea atar
+
+x=7
+x+=12
+print(x)
+
+#-= içerideki veriyi dışaridaki veri ile çıkarır ve sonucu variablea atar
+
+y=4
+y-=3
+print(y)
+
+#*= içerideki veriyi dışaridaki veri ile çarpar ve sonucu variablea atar
+
+z=4
+z*=3
+print(z)
